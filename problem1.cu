c0a0a12d34e0f4ba83810848cdcd634fddc75609
P37r <petershih11@gmail.com>
2024-04-21 20:21:12 -0500

    Update problem1.cu

--- problem1.cu
+++ problem1.cu
@@ -1,15 +1,10 @@
+
+
 #include <stdio.h>
 #include <math.h>
 #include <cuda_runtime.h>
 
-
-#include <iostream>
-#include <vector>
-#include <chrono>
-
-using namespace std::chrono;
-
-#define BLOCKSIZE 128
+#define BLOCKSIZE 256
 
 __global__ void partial_reduction(const int N, float *x_reduced, const float *x){
   
@@ -21,12 +16,11 @@ __global__ void partial_reduction(const int N, float *x_reduced, const float *x)
   // coalesced reads in
   s_x[tid] = 0.f;
   if (i < N){
-    s_x[tid] = x[i] + x[i + blockDim.x];
+    s_x[tid] = x[i]; + x[i + blockDim.x];
   }
 
   // number of "live" threads per block
-  int alive = blockDim.x /2;
-
+  int alive = blockDim.x/2;
   
   while (alive > 1){
     __syncthreads(); 
@@ -42,41 +36,6 @@ __global__ void partial_reduction(const int N, float *x_reduced, const float *x)
   }
 }
     
-// __global__ void partial_reduction(const int N, float *x_reduced, const float *x){
-  
-//   __shared__ float s_x[BLOCKSIZE];
-
-//   const int i = blockDim.x * 2*blockIdx.x + threadIdx.x;
-//   const int tid = threadIdx.x;
-  
-//   // coalesced reads in
-//   s_x[tid] = 0.f;
-//   if (i < N){
-//     s_x[tid] = x[i] + x[i + blockDim.x];
-//   }
-
-//   // number of "live" threads per block
-//   int alive = blockDim.x;
-//   s = 1
-//   while (alive > 1){
-//     __syncthreads(); 
-//     if (tid % (2*s) == 0){
-//       s_x[tid] += s_x[tid + s];
-//     }
-//     s *= 2;
-//     alive /= 2; // update the number of live threads    
-
-//   }
-
-
-//   // write out once we're done reducing each block
-//   if (tid==0){
-//     x_reduced[blockIdx.x] = s_x[0];
-//   }
-// }
-
-
-
 int main(int argc, char * argv[]){
 
   int N = 4096;
@@ -95,7 +54,7 @@ int main(int argc, char * argv[]){
   float * x_reduced = new float[numBlocks];  
 
   for (int i = 0; i < N; ++i){
-    x[i] = i + 1.f;
+    x[i] = 1.f;
   }
 
   // allocate memory and copy to the GPU
@@ -120,7 +79,8 @@ int main(int argc, char * argv[]){
     sum_x += x_reduced[i];
   }
 
-  float target = N * (N+1) / 2.f;
+  //  float target = N * (N+1) / 2.f;
+  float target = N;
   printf("error = %f\n", fabs(sum_x - target));
 
 #if 1
